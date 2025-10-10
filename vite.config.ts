53881a49e96c5cc52e593efef324d91ba6e78a78
Fajar Maulana <fajarma21@gmail.com>
2025-10-10 09:15:19 +0700

    fix(base): change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sprite-maker',
-  plugins: [react()],
+  base: '/sprite-sheet-maker',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  plugins: [react()],
 });
